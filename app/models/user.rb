6f694ac8711479cf255995c2172a908b1c222ebc
Ian Boucher <i.a.boucher@gmail.com>
2016-06-07 11:01:02 +0100

    Add user role selection to Devise forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:standard, :premium, :admin]
+  enum role: [:collector, :artist, :admin]
 end
